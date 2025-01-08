022a5c2cfc0a30a0ef9a07066d29df4f15768be6
Mohamed Ashref Ben Abdallah <109633107+Ashref-dev@users.noreply.github.com>
2025-01-08 03:48:35 +0100

    The way I feel — sometimes, it's too hard to sit still

--- components/common/loader.js
+++ components/common/loader.js
@@ -1,6 +1,19 @@
 'use client';
+import { useEffect, useState } from 'react';
 
 const LoadingScreen = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!isLoading) return null;
+
   return (
     <div className="loader-wrap">
       <svg viewBox="0 0 1000 1000" preserveAspectRatio="none">
